b2c8723e7d43607591e24ef92541d9a2255c4c9a
alexis <skodak95alexis@gmail.com>
2021-04-11 15:53:06 +0200

    Adds Eco2mix View and Line Graph component

---
+++ src/components/LineChart.vue
@@ -0,0 +1,62 @@
+<script>
+import {Line} from 'vue-chartjs/dist/vue-chartjs'
+
+export default {
+  extends: Line,
+  name: "LineChart",
+  props: {
+    dates: {
+      type: Array
+    },
+    mins: {
+      type: Array
+    },
+    maxs: {
+      type: Array
+    },
+    sums: {
+      type: Array
+    },
+    means: {
+      type: Array
+    },
+    options: {
+      type: Object
+    },
+  },
+  methods: {
+    renderLineChart: function () {
+      this.renderChart({
+        labels: this.dates,
+        datasets: [
+          {
+            data: this.mins,
+            label: "Min",
+            backgroundColor: 'rgb(207,244,85,0.3)',
+            borderColor: 'rgb(207,244,85)'
+          },
+          {
+            data: this.maxs,
+            label: "Max",
+            backgroundColor: 'rgb(244,135,85,0.3)',
+            borderColor: 'rgb(244,135,85)'
+          },
+          {
+            data: this.means,
+            label: "Moyenne",
+            backgroundColor: 'rgb(85,218,244, 0.3)',
+            borderColor: 'rgb(85,218,244)'
+          }
+        ]
+      }, this.options)
+    }
+  },
+  mounted() {
+    this.renderLineChart()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Eco2mix.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Eco2mix"
+}
+</script>
+
+<style scoped>
+
+</style>
